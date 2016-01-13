936f1b0e57fa66fa1d982bc156db28926b5e5682
Alanna Burke <alanna@chromaticsites.com>
2016-01-13 15:37:53 -0800

    Civil Comments skeleton.

---
+++ src/CivilCommentsAdminSettingsForm.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\civilcomments\Form\CivilCommentsAdminSettingsForm.
+ */
+
+namespace Drupal\civilcomments\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+
